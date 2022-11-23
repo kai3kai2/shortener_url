3f92fb1b9459d709b1dd280894aecb5408cfde5a
kai3kai2 <111210838+kai3kai2@users.noreply.github.com>
2022-11-23 15:59:39 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const { urlencoded } = require('express')
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
+mongoose.connect(process.env.MONGODB_URL)
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
---
+++ models/seeds/urlSeeder.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Url = require('../url')
+
+require('dotenv').config()
+
+
+mongoose.connect(process.env.MONGODB_URL)
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb seeder error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+
+  for (let i = 0; i < 4; i++) {
+    Url.create(
+      {
+        url: `https://localhost:3000/${i}`,
+        name: `test: ${i}`
+      }
+    )
+  }
+  console.log('done')
+})
+
